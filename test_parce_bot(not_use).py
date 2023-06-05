c0bbe617a72ae340952249348b902dd705729c0d
roman <whynot2288@gmail.com>
2023-06-05 22:19:01 +0300

    хвых

--- test_parce_bot(not_use).py
+++ test_parce_bot(not_use).py
@@ -11,7 +11,7 @@ from configurations import configuration_file as config
 api_id = config.configuration_data["TELETHON_API_ID"]
 api_hash = config.configuration_data["TELETHON_API_HASH"]
 username = config.configuration_data["TELETHON_USERNAME"]
-# group_href = ['https://t.me/drop_farsen', 'https://t.me/saledroppp']
+
 
 client = TelegramClient(username, api_id, api_hash)
 
@@ -20,34 +20,34 @@ members_str_username = []
 
 
 async def main(client):
-    with open("configurations/groups_members.txt") as file:
+    with open("configurations/groups_members.txt") as file:     # Открываем файл со списком групп для парсинга
         group_href = file.read().split(", ")
-    print(group_href)
-    # group_href = ['https://t.me/drop_farsen', 'https://t.me/saledroppp']
-    con = sqlite3.connect("logs/member_list.db")
+
+    con = sqlite3.connect("logs/member_list.db")    # Открываем db файл для записи спарсеных данных
     cur = con.cursor()
 
-    # for item_group in group_href:
-    #     namegroup = item_group.replace('https://t.me/', '')
-    #     cur.execute(f"CREATE TABLE IF NOT EXISTS {namegroup}(username TEXT,userid TEXT);")
+    # Проходим циклом по ссылкам групп в открытом выше файле
+    for item_group in group_href:  
+        namegroup = item_group.replace('https://t.me/', '')
+        cur.execute(f"CREATE TABLE IF NOT EXISTS {namegroup}(username TEXT,userid TEXT);")  # Создаем таблицу под каждую группу
         
-    #     chat_entity = await client.get_entity(item_group)
-    #     offset = 0
+        chat_entity = await client.get_entity(item_group)   # Получаем сущность для парсинга
+        offset = 0
 
-    #     print(f"Сейчас: {namegroup}")
-    #     while True:
-    #         participants =  await client(GetParticipantsRequest(chat_entity, filter=ChannelParticipantsSearch(q=""), offset=offset, limit=200, hash=200))
-    #         if not participants.users:
-    #             break
+        # Цикл для парсинга, сделан по гайдам, смещение offset каждые 200 пользователей
+        while True:
+            participants =  await client(GetParticipantsRequest(chat_entity, filter=ChannelParticipantsSearch(q=""), offset=offset, limit=200, hash=200))
+            if not participants.users:
+                break
             
-    #         for user in participants.users:
-    #             members_str_username.extend((user.username, user.id))
-    #             cur.execute(f"INSERT INTO {namegroup} VALUES(?, ?);", members_str_username)
-    #             members_str_username.clear()
-    #         offset += int(len(participants.users))
-    #         print(offset)
+            # Костыльная, но рабочая запись спарсшеных данных
+            for user in participants.users:
+                members_str_username.extend((user.username, user.id))
+                cur.execute(f"INSERT INTO {namegroup} VALUES(?, ?);", members_str_username)
+                members_str_username.clear()
+            offset += int(len(participants.users))
         
-    # con.commit()
+    con.commit()    # Закрываем SQL
         
 
 
